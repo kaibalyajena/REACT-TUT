80d018f6074b458bd9a6818627dfe466240b091f
kaibalya jena <kaibalya18j@gmail.com>
2023-03-28 01:32:51 +0530

    basic tic tac toe game

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -10,13 +10,33 @@ function Square({val,onSquareClick}){
 
 export default function TicTacToe(){
     const [squares,setSquares]=useState(Array(9).fill(null))
+    const [xturn,setXturn] = useState(true)
     function handleClick(i){
+        if(squares[i] && calculateWinner(squares)){
+            return;
+        }
         const newSquares=squares.slice();
-        newSquares[i]="X";
+        if(xturn===true && squares[i]===null){
+            newSquares[i]="X";
+        }else if(xturn===false && squares[i]===null)(
+            newSquares[i]="0"
+        )
         setSquares(newSquares)
+        setXturn(!xturn)
+    }
+    let status;
+    if(calculateWinner(squares)===null){
+        if(xturn===true){
+            status="X has to move now"
+        }else{
+            status="O has to move now"
+        }
+    }else{
+        status=calculateWinner(squares)+" is the winner"
     }
     return (
         <>
+        <p style={{fontWeight:"bold"}}>{status}</p>
         <div className="board-row">
             <Square val={squares[0]} onSquareClick={()=>handleClick(0)}/>
             <Square val={squares[1]} onSquareClick={()=>handleClick(1)}/>
@@ -35,3 +55,23 @@ export default function TicTacToe(){
         </>
     )
 }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
